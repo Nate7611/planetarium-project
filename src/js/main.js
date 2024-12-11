86c9516331265859820dceffb039ecfb38eaad1e
Nate Downey <natedowneymain@gmail.com>
2024-12-11 06:36:14 -0500

    Add a fast travel button

--- src/js/main.js
+++ src/js/main.js
@@ -21,6 +21,8 @@ const timeSlowButton = document.getElementById('slow-button');
 const timeFastButton = document.getElementById('fast-button');
 const unitSwitchButton = document.getElementById('unit-button');
 const unitSwitchButtonText = document.getElementById('unit-button__text');
+const fastTravelButton = document.getElementById('travel-button');
+const fastTravelElement = document.getElementById('fast-travel');
 
 // Loading UI
 const loadingScreen = document.getElementById('loading-screen');
@@ -222,17 +224,18 @@ const timer = new Timer();
 const planetHeightOffset = 0.00003;
 const maxDistance = 0.000004;
 
+// Just tragic
 const planets = [
-  { name: 'Mercury', arrived: false, left: false, position: 41.596, endPosition: 41.604, diameter: '3,032 mi (4,879 km)', temperature: '333°F (167°C)', moons: '0', dayLength: '175.9 Earth Days', funFact: "Mercury's temperature fluctuates from around 800°F (430°C) during the day to around -290°F (-180°C) at night." },
-  { name: 'Venus', arrived: false, left: false, position: 77.723, endPosition: 77.737, diameter: '7,521 mi (12,104 km)', temperature: '867°F (464°C)', moons: '0', dayLength: '116.8 Earth Days', funFact: 'Venus has a thick atmosphere of carbon dioxide that makes it hotter than Mercury, despite being farther from the Sun.' },
-  { name: 'Earth', arrived: false, left: false, position: 107.492, endPosition: 107.508, diameter: '7,926 mi (12,756 km)', temperature: '59°F (15°C)', moons: '1', dayLength: '1 Earth Day', funFact: 'Earth is the only planet in the Universe known to support life.' },
-  { name: 'The Moon', arrived: false, left: false, position: 107.774, endPosition: 107.778402704, diameter: '2,159 mi (3,475 km)', temperature: '-4°F (-20°C)', moons: '0', dayLength: '29.5 Earth Days', funFact: "When the Moon is at its farthest distance from Earth, you could fit all the other planets (including Pluto) in the space between them." },
-  { name: 'Mars', arrived: false, left: false, position: 163.696, endPosition: 163.704, diameter: '4,221 mi (6,792 km)', temperature: '-85°F (-65°C)', moons: '2', dayLength: '1.03 Earth Days', funFact: 'Mars is home to the tallest volcano in the Solar System, Olympus Mons, which is about two and a half times the height of Mount Everest.' },
-  { name: 'Jupiter', arrived: false, left: false, position: 559.2, endPosition: 559.4, diameter: '88,846 mi (142,984 km)', temperature: '-166°F (-110°C)', moons: '95', dayLength: '0.414 Earth Days', funFact: "Jupiter actually has rings, along with Uranus and Neptune. These rings are just much harder to see compared to Saturn's rings." },
-  { name: 'Saturn', arrived: false, left: false, position: 1028.88, endPosition: 1029.12, diameter: '74,897 mi (120,536 km)', temperature: '-220°F (-140°C)', moons: '146', dayLength: '0.444 Earth Days', funFact: "Despite Saturn's rings being around 175,000 miles (282,000 km) wide, their thickness is less than the length of a football field." },
-  { name: 'Uranus', arrived: false, left: false, position: 2066.968, endPosition: 2067.032, diameter: '31,763 mi (51,118 km)', temperature: '-320°F (-195°C)', moons: '27', dayLength: '0.718 Earth Days', funFact: 'Uranus is unique because it rotates on its side, with its axis tilted by about 98 degrees.' },
-  { name: 'Neptune', arrived: false, left: false, position: 3234.968, endPosition: 3235.032, diameter: '30,775 mi (49,528 km)', temperature: '-330°F (-200°C)', moons: '14', dayLength: '0.671 Earth Days', funFact: 'Neptune is known for its strong winds, the fastest in the Solar System, reaching speeds of more than 1,200 mph (2,000 km/h).' },
-  { name: 'Pluto', arrived: false, left: false, position: 4219.9978, endPosition: 4220.0022, diameter: '1,476 mi (2,376 km)', temperature: '-375°F (-225°C)', moons: '5', dayLength: '6.39 Earth Days', funFact: 'Pluto was once considered the ninth planet in our Solar System before being reclassified as a dwarf planet in 2006.' }
+  { name: 'Mercury', arrived: false, left: false, position: 41.596, endPosition: 41.604, diameter: '3,032 mi (4,879 km)', temperature: '333°F (167°C)', moons: '0', dayLength: '175.9 Earth Days', funFact: "Mercury's temperature fluctuates from around 800°F (430°C) during the day to around -290°F (-180°C) at night.", button: document.getElementById('discovered-planets__mercury') },
+  { name: 'Venus', arrived: false, left: false, position: 77.723, endPosition: 77.737, diameter: '7,521 mi (12,104 km)', temperature: '867°F (464°C)', moons: '0', dayLength: '116.8 Earth Days', funFact: 'Venus has a thick atmosphere of carbon dioxide that makes it hotter than Mercury, despite being farther from the Sun.', button: document.getElementById('discovered-planets__venus') },
+  { name: 'Earth', arrived: false, left: false, position: 107.492, endPosition: 107.508, diameter: '7,926 mi (12,756 km)', temperature: '59°F (15°C)', moons: '1', dayLength: '1 Earth Day', funFact: 'Earth is the only planet in the Universe known to support life.', button: document.getElementById('discovered-planets__earth') },
+  { name: 'The Moon', arrived: false, left: false, position: 107.774, endPosition: 107.778402704, diameter: '2,159 mi (3,475 km)', temperature: '-4°F (-20°C)', moons: '0', dayLength: '29.5 Earth Days', funFact: "When the Moon is at its farthest distance from Earth, you could fit all the other planets (including Pluto) in the space between them.", button: document.getElementById('discovered-planets__moon') },
+  { name: 'Mars', arrived: false, left: false, position: 163.696, endPosition: 163.704, diameter: '4,221 mi (6,792 km)', temperature: '-85°F (-65°C)', moons: '2', dayLength: '1.03 Earth Days', funFact: 'Mars is home to the tallest volcano in the Solar System, Olympus Mons, which is about two and a half times the height of Mount Everest.', button: document.getElementById('discovered-planets__mars') },
+  { name: 'Jupiter', arrived: false, left: false, position: 559.2, endPosition: 559.4, diameter: '88,846 mi (142,984 km)', temperature: '-166°F (-110°C)', moons: '95', dayLength: '0.414 Earth Days', funFact: "Jupiter actually has rings, along with Uranus and Neptune. These rings are just much harder to see compared to Saturn's rings.", button: document.getElementById('discovered-planets__jupiter') },
+  { name: 'Saturn', arrived: false, left: false, position: 1028.88, endPosition: 1029.12, diameter: '74,897 mi (120,536 km)', temperature: '-220°F (-140°C)', moons: '146', dayLength: '0.444 Earth Days', funFact: "Despite Saturn's rings being around 175,000 miles (282,000 km) wide, their thickness is less than the length of a football field.", button: document.getElementById('discovered-planets__saturn') },
+  { name: 'Uranus', arrived: false, left: false, position: 2066.968, endPosition: 2067.032, diameter: '31,763 mi (51,118 km)', temperature: '-320°F (-195°C)', moons: '27', dayLength: '0.718 Earth Days', funFact: 'Uranus is unique because it rotates on its side, with its axis tilted by about 98 degrees.', button: document.getElementById('discovered-planets__uranus') },
+  { name: 'Neptune', arrived: false, left: false, position: 3234.968, endPosition: 3235.032, diameter: '30,775 mi (49,528 km)', temperature: '-330°F (-200°C)', moons: '14', dayLength: '0.671 Earth Days', funFact: 'Neptune is known for its strong winds, the fastest in the Solar System, reaching speeds of more than 1,200 mph (2,000 km/h).', button: document.getElementById('discovered-planets__neptune') },
+  { name: 'Pluto', arrived: false, left: false, position: 4219.9978, endPosition: 4220.0022, diameter: '1,476 mi (2,376 km)', temperature: '-375°F (-225°C)', moons: '5', dayLength: '6.39 Earth Days', funFact: 'Pluto was once considered the ninth planet in our Solar System before being reclassified as a dwarf planet in 2006.', button: document.getElementById('discovered-planets__pluto') }
 ];
 
 const timeScales = [
@@ -247,6 +250,41 @@ const timeScales = [
   { speed: 5184000, label: '1s = 60d', soundVolume: 0.56 },
   { speed: 10368000, label: '1s = 120d', soundVolume: 0.64 },
 ];
+
+const fastTravel = {
+  mercury: document.getElementById('discovered-planets__mercury'),
+  venus: document.getElementById('discovered-planets__venus'),
+  earth: document.getElementById('discovered-planets__earth'),
+  moon: document.getElementById('discovered-planets__moon'),
+  mars: document.getElementById('discovered-planets__mars'),
+  jupiter: document.getElementById('discovered-planets__jupiter'),
+  saturn: document.getElementById('discovered-planets__saturn'),
+  uranus: document.getElementById('discovered-planets__uranus'),
+  neptune: document.getElementById('discovered-planets__neptune'),
+  pluto: document.getElementById('discovered-planets__pluto')
+};
+
+// Really hacky implementation but it is due in a few hours...
+for (const i in fastTravel) {
+  fastTravel[i].addEventListener('click', function() {
+    const planet = planets.find(p => p.name.toLowerCase() === i); // Match planets name
+
+    // Since the moon is not just named moon it wont find a match in the search hench the else
+    if (planet) {
+      planet.arrived = false;
+      planet.left = false;
+      voyagerModel.position.z = planet.position;
+      camera.position.z = planet.position - 1;
+    }
+    else {
+      voyagerModel.position.z = 107.774; // 107.774 is the moons start position
+      camera.position.z = 107.774 - 1;
+    }
+
+    fastTravelElement.style.display = 'none';
+    timeScale = 1;
+  });
+}
 
 // Audio variable
 let listener;
@@ -523,6 +561,16 @@ timeSlowButton.addEventListener('animationend', () => {
   timeSlowButton.style.animationName = 'none';
 });
 
+fastTravelButton.addEventListener('click', () => {
+  if (fastTravelElement.style.display == 'block') {
+    fastTravelElement.style.display = 'none';
+  }
+  else {
+    fastTravelElement.style.display = 'block';
+    fastTravelElement.style.animationName = 'reveal';
+  }
+});
+
 // Switch units and button text when pressed
 unitSwitchButton.addEventListener('click', function () {
   if (!usingMetric) {
@@ -778,6 +826,10 @@ function animate() {
     // Check if we left planet
     if (planets[i].arrived && !planets[i].left && voyagerModel.position.z >= planets[i].endPosition) {
       planets[i].left = true;
+
+      // Enable fast travel buttons
+      planets[i].button.style.opacity = '1';
+      planets[i].button.style.pointerEvents = 'auto';
 
       // Fade out factContainer
       factsContainer.style.animationName = 'hide';
